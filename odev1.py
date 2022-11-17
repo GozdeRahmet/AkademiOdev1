383fa450d4180f66872c47d42f6bcfac6e44a832
Gözde Rahmet (ETIYA) <gozde.rahmet@etiya.com>
2022-11-17 11:43:09 +0300

    odev1 odev2

---
+++ odev1.py
@@ -0,0 +1,12 @@
+
+sayi1 = 1
+sayi2 = 2
+
+if sayi1 > sayi2:
+  print (sayi1)
+  
+elif sayi2 > sayi1:
+  print (sayi2)
+  
+else:
+  print( )
---
+++ odev2.py
@@ -0,0 +1,21 @@
+
+
+sayi1 = 1
+sayi2 = 2
+sayi3 = 3
+
+if sayi1 < sayi2:
+  if sayi1 < sayi3:
+    print (sayi1)
+
+elif sayi2 < sayi3:
+      print (sayi2)
+else:
+  print(sayi3)
+
+if sayi1 > sayi2:
+  if sayi1 > sayi3:
+    print (sayi1)
+
+elif sayi2 > sayi3:
+      print (sayi2)
